a8fbd6a367db5e5076528fb9b0ca41d415618660
Thomas Pequegnot <thomas.pequegnot04@gmail.com>
2025-04-05 14:23:23 -0400

    add descriptions for pages

--- app.py
+++ app.py
@@ -25,4 +25,16 @@ pg = st.navigation([
     st.Page("pages/7_Traffic_vs_Temperature.py"),
     st.Page("pages/6_Traffic_TS_Analysis.py")
 ])
+
+# Add descriptions for each page
+page_descriptions = {
+    "pages/2_Top_Entries_Analytics.py": "View analytics for the top entries in the dataset.",
+    "pages/3_Interactive_Map.py": "Visualize congestion data on an interactive map.",
+    "pages/4_Vehicle_Timeseries.py": "Analyze vehicle trends over time using time series data.",
+    "pages/5_Vehicle_Class_Breakdown.py": "Analyze vehicle data by class, comparing peak hours to overnight trends.",
+    "pages/8_Clustering_Labels.py": "Explore clustering labels applied to the dataset to analyze changes in CRZ during peak and low-demand periods, comparing weekdays and weekends.",
+    "pages/7_Traffic_vs_Temperature.py": "Analyze the relationship of CRZ, temperature, and precipitation in 3 dimensions.",
+    "pages/6_Traffic_TS_Analysis.py": "Perform daily CRZ entries over time, categorized by weather conditions (Favorable, Unfavorable, Neutral), highlighting traffic volume trends and fluctuations.",
+}
+
 pg.run()
